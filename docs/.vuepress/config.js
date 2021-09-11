b0ef61e971387acd7b6ac7ef4f6e85bbda12560a
Subilan <suyuanliang1995@gmail.com>
2021-09-11 18:01:54 +0800

    ft: add time

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,7 +27,7 @@ module.exports = {
 			{ text: '官网', link: 'https://seatide.top' }
 		],
 		sidebar: sidebar(),
-		// lastUpdated: '最后更新于',
+		lastUpdated: '最后更新于',
 		search: true,
 		searchMaxSuggestions: 10,
 		editLinks: true,
@@ -46,21 +46,17 @@ module.exports = {
 		mathjax: {
 			target: 'chtml'
 		},
-		/* '@vuepress/last-updated': {
+		'@vuepress/last-updated': {
 			transformer: (timestamp, lang) => {
 				const moment = require('moment');
 				moment.locale('zh-CN');
 				const m = moment(timestamp);
-				// if (Math.abs(new Date().getTime() - m.toDate().getTime()) < 604800000) {
-				// 	return m.fromNow();
-				// }
-                                return m.fromNow();
-				// return m.toDate().toLocaleDateString();
+				if (Math.abs(new Date().getTime() - m.toDate().getTime()) < 604800000) {
+					return m.fromNow();
+				}
+				return m.toDate().toLocaleDateString();
 			},
-			dateOptions: {
-				hour12: false
-			}
-		} */
+		}
 	},
 	markdown: {
 		plugins: ['footnote', 'sup', 'sub']
